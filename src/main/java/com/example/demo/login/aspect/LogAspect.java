64bb16e8738ba25563204ee84ac19247cf190866
kondo takumi <s.093.kondo@gmail.com>
2020-01-07 19:36:15 +0900

    ログ出力の実装

---
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -0,0 +1,28 @@
+package com.example.demo.login.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+	@Around("execution(* *..*.*Controller.*(..))")
+	public Object statLog(ProceedingJoinPoint joinPoint) throws Throwable{
+		System.out.println("メソッド開始：" + joinPoint.getSignature());
+
+		try {
+			Object result = joinPoint.proceed();
+			System.out.println("メソッド終了：" + joinPoint.getSignature());
+			return result;
+		}catch(Exception e) {
+			System.out.println("メソッド異常終了：" + joinPoint.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+}
+
